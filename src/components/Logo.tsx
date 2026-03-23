cc85251db66b44eb5e99fb51b60f11601058923e
levardomangilit-rgb <levardomangilit@gmail.com>
2026-03-23 17:36:38 +0800

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-
-interface LogoProps {
-  className?: string;
-}
+import logoSrc from '../assets/logo.png';
 
 const Logo: React.FC<LogoProps> = ({ className = '' }) => (
   <img
-    src="/src/assets/logo.png"
+    src={logoSrc}
     alt="PhilTech Logo"
     className={`h-10 w-10 object-contain ${className}`}
   />
 );
-
-export { Logo };
